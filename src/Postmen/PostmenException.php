44f46b996475ed02561442f8e6dedcce3e5efeee
Dominik Dera <dominik.dera@printed.com>
2021-04-01 10:17:46 +0100

    Pass potential api response data to PostmenException.
    This is needed in case an api call to the /rates endpoint returns error details in the response data itself, rather than in the "meta" hashmap. This is usually the case for the 4713 error code.

--- src/Postmen/PostmenException.php
+++ src/Postmen/PostmenException.php
@@ -13,10 +13,18 @@ class PostmenException extends Exception
 {
 	private $retryable;
 	private $details;
-	
-	public function __construct($message, $code, $retryable, $details, Exception $previous = null) {
+
+	/**
+	 * @var \stdClass|array|null If the error happened during running an api request and the api responded, then this
+	 *  is the related response. Depending on whether the requests was done with "array: true/false" parameter, the response
+	 *  is either a \stdClass object or an array.
+	 */
+	private $response_data;
+
+	public function __construct($message, $code, $retryable, $details, Exception $previous = null, $response_data = null) {
 		$this->retryable = $retryable;
 		$this->details = $details;
+		$this->response_data = $response_data;
 		parent::__construct($message, $code, $previous);
 	}
 
@@ -26,6 +34,10 @@ class PostmenException extends Exception
 
 	public function getDetails() {
 		return $this->details;
+	}
+
+	public function getResponseData() {
+		return $this->response_data;
 	}
 
 }
